a3a3dd176eb8e46a7fd3a67611a3ba16f26489d1
Abdelmuttalib Ahmed <ahmed.invix@gmail.com>
2023-08-12 22:39:01 +0800

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,189 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { useState } from "react";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "react-beautiful-dnd";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Board />
     </main>
-  )
+  );
+}
+
+interface TTask {
+  id: string;
+  title: string;
+  description: string;
+  assigneeName: string;
+  assigneeAvatarUrl: string;
+  status: "todo" | "inProgress" | "done";
+  orderIndex: number;
+}
+
+const initialTasks: TTask[] = [
+  {
+    id: "1",
+    title: "Implement user authentication",
+    description:
+      "Create a user authentication system using Firebase Authentication.",
+    assigneeName: "John Doe",
+    assigneeAvatarUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    status: "todo",
+    orderIndex: 0,
+  },
+  {
+    id: "2",
+    title: "Design landing page",
+    description: "Design a landing page for the website using Figma or Sketch.",
+    assigneeName: "Jane Smith",
+    assigneeAvatarUrl:
+      "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    status: "todo",
+    orderIndex: 1,
+  },
+  {
+    id: "3",
+    title: "Setup Redux store",
+    description: "Setup Redux store for managing state in the application.",
+    assigneeName: "John Doe",
+    assigneeAvatarUrl:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    status: "inProgress",
+    orderIndex: 0,
+  },
+  {
+    id: "4",
+    title: "Implement task board UI",
+    description: "Implement the task board UI using React and Tailwind CSS.",
+    assigneeName: "Jane Smith",
+    assigneeAvatarUrl:
+      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    status: "inProgress",
+    orderIndex: 1,
+  },
+  {
+    id: "5",
+    title: "Add unit tests",
+    description: "Add unit tests for the application using Jest and Enzyme.",
+    assigneeName: "John Doe",
+    assigneeAvatarUrl:
+      "https://images.unsplash.com/photo-1568822617270-2c1579f8dfe2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    status: "done",
+    orderIndex: 0,
+  },
+  {
+    id: "6",
+    title: "Deploy application to AWS",
+    description: "Deploy the application to AWS using EC2 and S3.",
+    assigneeName: "Jane Smith",
+    assigneeAvatarUrl:
+      "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    status: "done",
+    orderIndex: 1,
+  },
+];
+import { resetServerContext } from "react-beautiful-dnd";
+
+function Board() {
+  resetServerContext();
+  const [tasks, setTasks] = useState<TTask[]>(
+    initialTasks.sort((a, b) => a.orderIndex - b.orderIndex)
+  );
+  const statuses = ["todo", "inProgress", "done"];
+
+  function handleOnDragEnd(result: DropResult) {
+    console.log("result", result);
+  }
+
+  return (
+    <div className="flex justify-center">
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <div className="flex flex-col lg:flex-row gap-6 w-full min-h-[100svh]">
+          {JSON.stringify(tasks)}
+          {/* Column/Status */}
+          {statuses.map((status) => {
+            const tasksByStatus = tasks.filter(
+              (task) => task.status === status
+            );
+            return (
+              <div
+                key={status}
+                className="w-full lg:w-1/3 p-4 bg-gray-100/80 h-full"
+              >
+                <h2 className="text-lg font-bold mb-4">{status}</h2>
+                <Droppable droppableId={status}>
+                  {(provided) => (
+                    <div
+                      className="space-y-4"
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                    >
+                      {/* Tasks */}
+                      {tasksByStatus.map((task, index) => (
+                        <Draggable
+                          key={task.id.toString()}
+                          draggableId={task.id.toString()}
+                          index={index}
+                        >
+                          {(provided) => (
+                            <div
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              ref={provided.innerRef}
+                              className="border rounded-lg p-4 mb-4 bg-white"
+                            >
+                              <h3 className="font-bold text-lg mb-2">
+                                {task.title}
+                              </h3>
+                              <p className="text-gray-700">
+                                {task.description}
+                              </p>
+                              <div className="flex justify-between items-center gap-2 mt-4">
+                                <div className="flex items-center">
+                                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                                  <img
+                                    className="w-10 h-10 rounded-full mr-2 object-cover"
+                                    src={task.assigneeAvatarUrl}
+                                    alt="Assignee avatar"
+                                  />
+                                  <span className="text-gray-700">
+                                    {task.assigneeName}
+                                  </span>
+                                </div>
+                                <span
+                                  className={`px-2 py-1 rounded-full font-bold text-sm ${
+                                    task.status === "done"
+                                      ? "bg-green-500 text-white"
+                                      : "bg-gray-300 text-gray-700"
+                                  }`}
+                                >
+                                  {task.status}
+                                </span>
+                              </div>
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </div>
+            );
+          })}
+        </div>
+      </DragDropContext>
+    </div>
+  );
 }
